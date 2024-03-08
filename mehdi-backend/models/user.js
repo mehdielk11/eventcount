6d3da1e133510a24cd098c15a3ab500a7e79ef0e
mehdi <elkhemlichi.mehdi@gmail.com>
2024-03-08 01:37:56 +0100

    user.js

---
+++ mehdi-backend/models/user.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+// Define the User schema
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+});
+
+// Create the User model
+const User = mongoose.model('User', userSchema);
+
+
+export default User;
